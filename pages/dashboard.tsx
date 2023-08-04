896b190f7c34a29e65d9eb92a2ebb88dbab57849
Eric Sheen <eric.sprog@gmail.com>
2023-08-04 16:16:26 -0400

    redirect

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -39,7 +39,7 @@ export async function getServerSideProps(context: any) {
     if (!session) {
         return {
           redirect: {
-            destination: '/api/auth/signin?callbackUrl=/dashboard',
+            destination: '/',
             permanent: false,
           },
         }
